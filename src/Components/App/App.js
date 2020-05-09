4f4a923544b4c36b9380639650cbf409c3c75519
Richard Ball <webalation@gmail.com>
2020-05-09 10:54:41 -0400

    Created all component file structures.

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return 
+  (
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist">
+          <SearchResults />
+          <Playlist />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
