a8bb21eb0481c0c38127461c82ef676287293946
Richard Ball <webalation@gmail.com>
2020-05-11 10:47:01 -0400

    Hardcoded values for the playlistTracks and searchResults arrays.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,7 +9,17 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      searchResults: []
+      searchResults: [{id: 1, name: 'Hello', artist: 'Kes', album: 'Folklore Riddim'},
+                      {id: 2, name: 'Tombstone', artist: 'Mandella Linkz', album: 'Mandrakes Riddim'},
+                      {id: 3, name: 'That Work (Look At You)', artist: 'Problem Child', album: 'Force It Riddim'},
+                      {id: 4, name: 'True Masquerader', artist: 'Kes', album: 'Titans Riddim'},
+                      {id: 5, name: 'Showtime', artist: 'Machel Montano', album: 'Pim Pim Riddim'}],
+      playlistName: 'Soca Brainwash Mix',
+      playlistTracks: [{id: 1, name: 'Play Harder', artist: 'Machel Montano', album: 'Pops Guitar Riddim'}, 
+                       {id: 2, name: 'Radar', artist: 'Kes', album: '4th Quarter Riddim'},
+                       {id: 3, name: 'Release', artist: 'Machel Montano', album: 'G.O.A.T.'},
+                       {id: 4, name: 'Dear Drunk People', artist: 'Kerwin Du Bois', album: 'Tingin Riddim'},
+                       {id: 5, name: 'The Best', artist: 'Sekon Sta', album: 'Kan Kan Riddim'}]
     }
   }
 
