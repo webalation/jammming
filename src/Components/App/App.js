18aa7e7f4503fa3d92b75566b37a57945904ff31
Richard Ball <webalation@gmail.com>
2020-05-12 19:57:53 -0400

    Completed updatePlaylistName method.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -24,6 +24,7 @@ class App extends React.Component {
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -48,6 +49,12 @@ class App extends React.Component {
     });
   }
 
+  updatePlaylistName(name) {
+    this.setState({
+      playlistName: name
+    });
+  }
+
   render() {
     return (
       <div>
@@ -59,7 +66,8 @@ class App extends React.Component {
                            onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} 
                       playlistTracks={this.state.playlistTracks}
-                      onRemove={this.removeTrack} />
+                      onRemove={this.removeTrack}
+                      onNameChange={this.updatePlaylistName} />
           </div>
         </div>
       </div>
