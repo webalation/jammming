d1ada2c9c23ca85ed066f3838eee9ac946b0fdaf
Richard Ball <webalation@gmail.com>
2020-05-15 12:59:47 -0400

    Updates

---
+++ precache-manifest.8a56c77d9ec46780b508aceb5016cbf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de030200a64cc62f23d70c2837042e6",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "dad24dea35142827eab5",
+    "url": "/jammming/static/css/main.910b6c53.chunk.css"
+  },
+  {
+    "revision": "eeee13a0ed6a817aa796",
+    "url": "/jammming/static/js/2.6d23d5a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jammming/static/js/2.6d23d5a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dad24dea35142827eab5",
+    "url": "/jammming/static/js/main.8163de83.chunk.js"
+  },
+  {
+    "revision": "e09f1e32bf8a78998d52",
+    "url": "/jammming/static/js/runtime-main.542e1b1d.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
