01dbe138a2cf3ef874cc93c503057c0e70a602d8
Richard Ball <webalation@gmail.com>
2020-05-18 18:41:46 -0400

    Completed feature: pressing enter triggers search.

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -11,6 +11,7 @@ class SearchBar extends React.Component {
 
 		this.search = this.search.bind(this);
 		this.handleTermChange = this.handleTermChange.bind(this);
+		this.handleKeyPress = this.handleKeyPress.bind(this);
 	}
 
 	search() {
@@ -23,11 +24,20 @@ class SearchBar extends React.Component {
 		});
 	}
 
+	handleKeyPress(event) {
+		if (event.key === 'Enter') {
+			this.search();
+		}
+	}
+
 	render() {
 		return (
 			<div className="SearchBar">
-  				<input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
-  				<button className="SearchButton" onClick={this.search}>SEARCH</button>
+  				<input placeholder="Enter A Song, Album, or Artist" 
+				  	   onChange={this.handleTermChange} 
+					   onKeyPress={this.handleKeyPress} />
+  				<button className="SearchButton" 
+				  		onClick={this.search}>SEARCH</button>
 			</div>
 		);
 	}
